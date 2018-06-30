7f16269d3f233344d8afc586c781f1290d206f80
Max Beutelspacher <max.beutelspacher@mailbox.org>
2018-06-30 15:22:05 +0200

    add tests and change to +/- prefixes

--- filetags.el
+++ filetags.el
@@ -25,26 +25,30 @@
 
 
 ;;; Code:
+(require 'seq)
+(require 'subr-x)
+(require 'cl)
 (defvar filetags-delimiter " -- " "delimiter between filename and tags")
+(defvar filetags-controlled-vocabulary '() "tags that can be added (besides tags which are already there)")
 
 (defun filetags-extract-filetags (filename)
   "extract the tags from FILENAME remove duplicates and sort them"
   (if (string-match-p (regexp-quote filetags-delimiter) (file-name-sans-extension filename))
-    (filetags-sort-and-uniq-tags 
-      (split-string 
-       (car (reverse (split-string (file-name-sans-extension filename) filetags-delimiter))) split-string-default-separators
+      (filetags-sort-and-uniq-tags
+       (split-string
+        (car (reverse (split-string (file-name-sans-extension filename) filetags-delimiter))) split-string-default-separators
         t split-string-default-separators)
-      )
+       )
     '()
     )
-)
+  )
 
 (defun filetags-sort-and-uniq-tags (tags)
   "sort TAGS and remove duplicates"
   (sort
-   (remove-duplicates tags :test 'string=)
+   (cl-remove-duplicates tags :test 'string=)
    'string<)
-)
+  )
 
 (defun filetags-extract-filename-without-tags(filename)
   "return filename without tags or delimiter or extension"
@@ -56,29 +60,25 @@
   (let ((new_filename (filetags-extract-filename-without-tags old_filename))
         (extension (file-name-extension old_filename)))
     (if tags
-    (concat new_filename filetags-delimiter (mapconcat 'identity (filetags-sort-and-uniq-tags tags) " ")  (if extension ".") extension)
-    (concat new_filename (if extension ".") extension)
-    )
+        (concat new_filename filetags-delimiter (mapconcat 'identity (filetags-sort-and-uniq-tags tags) " ")  (if extension ".") extension)
+      (concat new_filename (if extension ".") extension)
       )
     )
+  )
 
-(defun filetags-add-tags(fullname tags)
-    (let* ((old_tags (filetags-extract-filetags (file-name-nondirectory fullname)))
-          (all_tags (filetags-sort-and-uniq-tags (append tags old_tags)))
-          (new_file_name (filetags-generate-new-filename (file-name-nondirectory fullname) all_tags)))
-      (if (not (string= new_file_name (file-name-nondirectory fullname)))
-            (dired-rename-file fullname (concat (file-name-directory fullname) new_file_name) nil)
-        )
-     )
- )
+(defun filetags-add-tags-to-filename(fullname tags)
+  (let* ((old_tags (filetags-extract-filetags (file-name-nondirectory fullname)))
+         (all_tags (filetags-sort-and-uniq-tags (append tags old_tags)))
+         (new_file_name (filetags-generate-new-filename (file-name-nondirectory fullname) all_tags)))
+    (concat (file-name-directory fullname) new_file_name)
+    )
+  )
 
 (defun filetags-remove-tags(fullname tags)
   (let* ((old_tags (filetags-extract-filetags (file-name-nondirectory fullname)))
          (all_tags (filetags-sort-and-uniq-tags (cl-set-difference old_tags tags :test 'string=)))
          (new_file_name (filetags-generate-new-filename (file-name-nondirectory fullname) all_tags)))
-    (if (not (string= new_file_name (file-name-nondirectory fullname)))
-        (dired-rename-file fullname (concat (file-name-directory fullname) new_file_name) nil)
-      )
+    (concat (file-name-directory fullname) new_file_name)
     )
   )
 
@@ -88,14 +88,14 @@
       (let((filenames (dired-get-marked-files))
            (tags (split-string tagsstring split-string-default-separators t split-string-default-separators)))
         (dolist (filename filenames)
-          (filetags-add-tags filename tags)
+          (filetags-add-tags-to-filename filename tags)
           )
+        )
+    (let((fullname (dired-get-filename))
+         (tags (split-string tagsstring split-string-default-separators t split-string-default-separators)))
+      (filetags-add-tags-to-filename fullname tags)
       )
-  (let((fullname (dired-get-filename))
-        (tags (split-string tagsstring split-string-default-separators t split-string-default-separators)))
-    (filetags-add-tags fullname tags)
     )
-  )
   )
 
 (defun dired-remove-tags(tagsstring)
@@ -114,5 +114,74 @@
     )
   )
 
+(defun filetags-update-tags (fullname tags-with-prefix)
+  (let ((tags-to-add (filetags-filter-add-tags tags-with-prefix))
+        (tags-to-remove (filetags-filter-remove-tags tags-with-prefix))
+        (new-filename))
+    (setq new-filename (filetags-add-tags-to-filename fullname tags-to-add))
+    (filetags-remove-tags new-filename tags-to-remove)
+    )
+  )
+
+(defun filetags-filter-add-tags (tags-with-prefix)
+  (mapcar (lambda (str) (string-trim-left str "+")) (seq-filter (lambda (tag) (s-starts-with? "+" tag)) tags-with-prefix ))
+  )
+
+(defun filetags-filter-remove-tags (tags-with-prefix)
+  (mapcar (lambda (str) (string-trim-left str "-")) (seq-filter (lambda (tag) (s-starts-with? "-" tag)) tags-with-prefix ))
+  )
+
+(defun filetags-union(l)
+  (filetags-sort-and-uniq-tags (cond ((null l) nil)
+                              ((null (cdr l)) (car l))
+                              (t (append (car l) (filetags-union (cdr l))))) ))
+;; taken from https://stackoverflow.com/a/31422481
+(defun filetags-intersection(l &rest cl-keys)
+  (cond ((null l) nil)
+        ((null (cdr l)) (car l))
+        (t (apply 'cl-intersection (car l) (apply 'filetags-intersection (cdr l) cl-keys) cl-keys))))
+
+
+
+
+(defun filetags-accumulate-remove-tags-candidates (filenames)
+  (filetags-union (mapcar 'filetags-extract-filetags filenames))
+  )
+
+(defun filetags-accumulate-add-tags-candidates (filenames)
+  (let ((remove-tags (filetags-accumulate-remove-tags-candidates filenames))
+        (tags-in-every-file (filetags-intersection (mapcar 'filetags-extract-filetags filenames) :test 'string=)))
+    (set-difference remove-tags tags-in-every-file :test 'string=)
+    )
+  )
+;; (defun filetags-ivy-get-tag(tags)
+;;   "uses ivy to ask for a tag out of TAGS if ENDPROMT is chosen return nil otherwise return tag"
+;;   (let ((new-tag (ivy-read "Add Tags: " (push "ENDPROMPT" tags)))) 
+;;     (when (not (string= new-tag "ENDPROMPT"))
+;;       new-tag
+;;       )
+;;     )
+;;   )
+
+;; (defun filetags-collect-tags-ivy()
+;;   (interactive)
+;;   (let ((tags)
+;;         (prompt-finished))
+;;     (progn
+;;       (while (not prompt-finished)
+;;         (let((new_tag (ivy-read "Add Tags : " '("ENDPROMPT" "foo" "bar" "baz")) ))
+;;           (if (string= new_tag "ENDPROMPT")
+;;               (setq prompt-finished t)
+;;             (push new_tag tags))
+;;           )
+;;         )
+;;       )
+;;     )
+;;   )
+
 (provide 'filetags)
 ;;; filetags.el ends here
+
+;; (message (filetags-ivy-get-tag '("test")))
+
+;; (filetags-intersection '(("test1" "test2") ("test2") ("test2" "test3")) :test 'string=)
--- test/filetags-test.el
+++ test/filetags-test.el
@@ -2,6 +2,89 @@
 (load-file "filetags.el")
 (require 'filetags)
 
+(ert-deftest extract-filetags-from-filename-with-tags-and-extension-test ()
+  (should (equal (filetags-extract-filetags "test -- test1 test2 test3.pdf") '("test1" "test2" "test3")))
+  )
+
+(ert-deftest extract-filetags-from-filename-with-tags-and-without-extension-test ()
+  (should (equal (filetags-extract-filetags "test -- test1 test2 test3") '("test1" "test2" "test3")))
+  )
+
+(ert-deftest extract-filetags-from-filename-without-tags-test ()
+  (should (equal (filetags-extract-filetags "test.pdf") nil))
+  )
+
+(ert-deftest extracted-filetags-are-a-set-test ()
+  (should (equal (filetags-extract-filetags "test -- test1 test1 test3.pdf") '("test1" "test3")))
+  )
+
+(ert-deftest extracted-filetags-are-a-sorted-test ()
+  (should (equal (filetags-extract-filetags "test -- test4 test1 test3.pdf") '("test1" "test3" "test4")))
+  )
+
+(ert-deftest extracted-filetags-whitespace-is-ignored-test ()
+  (should (equal (filetags-extract-filetags "test --    test1       test2.pdf") '("test1" "test2")))
+  )
+
+
+(ert-deftest filetags-update-tags-adds-tags-with-plus-test()
+  (let ((fullname "/home/max/test.txt")
+        (expected-fullname "/home/max/test -- test1.txt"))
+    (should (equal (filetags-update-tags fullname '("+test1")) expected-fullname))
+    )
+  )
+
+(ert-deftest filetags-update-tags-remove-tags-with-minus-test()
+  (let ((fullname "/home/max/test -- test1.txt")
+        (expected-fullname "/home/max/test.txt"))
+    (should (equal (filetags-update-tags fullname '("-test1")) expected-fullname))
+    )
+  )
+
+(ert-deftest filetags-update-tags-removes-and-adds-simoultaniously-test()
+  (let ((fullname "/home/max/test -- test1.txt")
+        (expected-fullname "/home/max/test -- test2.txt"))
+    (should (equal (filetags-update-tags fullname '("-test1" "+test2")) expected-fullname))
+    )
+  )
+
+(ert-deftest filetags-update-tags-ignores-non-action-tags-test()
+  (let ((fullname "/home/max/test -- test1.txt")
+        (expected-fullname "/home/max/test.txt"))
+    (should (equal (filetags-update-tags fullname '("-test1" "not-actionable")) expected-fullname))
+    )
+  )
+
+(ert-deftest filetags-filter-add-tags-test()
+  (let ((tags-with-prefix '("+test1" "-test2" "test3"))
+        (expected-add-tags '("test1")))
+    (should (equal (filetags-filter-add-tags tags-with-prefix) expected-add-tags))
+    )
+  )
+(ert-deftest filetags-filter-remove-tags-test()
+  (let ((tags-with-prefix '("+test1" "-test2" "test3"))
+        (expected-remove-tags '("test2")))
+    (should (equal (filetags-filter-remove-tags tags-with-prefix) expected-remove-tags))
+    )
+  )
+
+(ert-deftest filetags-accumulate-remove-tags-candidates-are-sorted-union-of-tags-test ()
+  (let ((filenames '("/home/max/test -- test3.txt" "/home/max/test -- test2 test1.txt" "/home/max/test -- test2.txt" ))
+        (expected-remove-candidates '("test1" "test2" "test3")))
+    (should (equal expected-remove-candidates (filetags-accumulate-remove-tags-candidates filenames)))
+    )
+  )
+
+(ert-deftest filetags-accumulate-add-tags-candidates-are-sorted-union-of-tags-minus-tags-which-are-in-every-file-test ()
+  (let ((filenames '("/home/max/test -- test2 test3.txt" "/home/max/test -- test2 test1.txt" "/home/max/test -- test2.txt" ))
+        (expected-add-candidates '("test1" "test3")))
+    (should (equal expected-add-candidates (filetags-accumulate-add-tags-candidates filenames)))
+    )
+  )
+
+
+
+
 
 
 
